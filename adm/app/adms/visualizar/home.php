b67d59460a032a6a6d636c02f35df33646c0d5a0
natobpia <natobpia@hotmail.com>
2020-07-08 16:03:34 -0300

    Menu vertical no administrativo

--- adm/app/adms/visualizar/home.php
+++ adm/app/adms/visualizar/home.php
@@ -7,6 +7,13 @@ include_once 'app/adms/include/head.php';
 <body>
     <?php
     include_once 'app/adms/include/header.php';
+    ?>
+    <div class="d-flex">
+    <?php
+    include_once 'app/adms/include/menu.php';
+    ?>   
+    </div>
+    <?php
     echo "Bem-vindo a home<br>";
     echo "<a href='" . pg . "/acesso/sair'>Sair</a><br>";
     include_once 'app/adms/include/rodape_lib.php';
