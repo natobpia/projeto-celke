0256c9650f97a0120781cfd1a7d677c814121e94
natobpia <natobpia@hotmail.com>
2020-07-09 10:45:02 -0300

    Liberar e bloquear itens do menu vertical

--- adm/app/adms/visualizar/home.php
+++ adm/app/adms/visualizar/home.php
@@ -9,15 +9,16 @@ include_once 'app/adms/include/head.php';
     include_once 'app/adms/include/header.php';
     ?>
     <div class="d-flex">
-    <?php
-    include_once 'app/adms/include/menu.php';
-    ?>   
-    </div>
-    <?php
-    echo "Bem-vindo a home<br>";
-    echo "<a href='" . pg . "/acesso/sair'>Sair</a><br>";
-    include_once 'app/adms/include/rodape_lib.php';
-    ?>
+        <?php
+        include_once 'app/adms/include/menu.php';
 
+        echo "Bem-vindo a home<br>";
+        echo "<a href='" . pg . "/acesso/sair'>Sair</a><br>";
+
+        
+
+        include_once 'app/adms/include/rodape_lib.php';
+        ?>
+    </div>
 </body>
 
