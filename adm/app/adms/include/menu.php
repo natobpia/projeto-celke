a863d697eb4a1efaec5839dbb80f5d9ac2bcb2f5
natobpia <natobpia@hotmail.com>
2020-07-13 10:04:01 -0300

    Imprimir o menu dropdown com PHP

--- adm/app/adms/include/menu.php
+++ adm/app/adms/include/menu.php
@@ -2,7 +2,7 @@
     <ul class="list-unstyled">
         <?php
         $result_menu = "SELECT nivpg.*,
-        men.nome, men.icone iconmen,
+        men.id id_menu, men.nome, men.icone iconmen,
         pg.nome_pagina, pg.endereco, pg.icone iconpg
         FROM adms_nivacs_pgs nivpg
         INNER JOIN adms_menus men ON men.id=nivpg.adms_menu_id
@@ -12,30 +12,45 @@
         AND nivpg.lib_menu=1";
         $resultado_menu = mysqli_query($conn, $result_menu);
 
+        $cont_drop = 0;
+        $cont_drop_fech = 0;
         while ($row_menu = mysqli_fetch_assoc($resultado_menu)) {
             if ($row_menu['dropdown'] == 1) {
+                if ($cont_drop != $row_menu['id_menu']) {
+
+                    if ($cont_drop_fech == 1 AND $cont_drop != 0) {
+                        echo "</ul>";
+                        echo "</li>";
+                    }
+                    echo "<li>";
+                    echo "<a href = '#submenu" . $row_menu['id_menu'] . "' data-toggle = 'collapse'>";
+                    echo "<i class = '" . $row_menu['iconmen'] . "'></i> " . $row_menu['nome'] . "";
+                    echo "</a>";
+                    echo "<ul id = 'submenu" . $row_menu['id_menu'] . "' class = 'list-unstyled collapse'>";
+                    $cont_drop = $row_menu['id_menu'];
+                }
+                echo "<li><a href='" . pg . "/" . $row_menu['endereco'] . "'>
+            <i class='" . $row_menu['iconpg'] . "'></i> " . $row_menu['nome_pagina'] . "</a></li>";
                 ?>
-                <li>
-                    <a href = "#submenu1" data-toggle = "collapse">
-                        <i class = "fas fa-user"></i> Usuário
-                    </a>
-                    <ul id = "submenu1" class = "list-unstyled collapse">
-                        <li><a href = "listar.html"><i class = "fas fa-users"></i> Usuários</a></li>
-                        <li><a href = "#"><i class = "fas fa-key"></i> Nível de Acesso</a></li>
-                    </ul>
-                </li>
+
                 <?php
+                $cont_drop_fech = 1;
             } else {
                 echo "<li><a href='" . pg . "/" . $row_menu['endereco'] . "'>
-            <i class='" . $row_menu['iconmen'] . "'></i> " . $row_menu['nome'] . "</a></li>";
+            <i class='" . $row_menu['iconpg'] . "'></i> " . $row_menu['nome_pagina'] . "</a></li>";
             }
+        }
+
+        if ($cont_drop_fech == 1) {
+            echo "</ul>";
+            echo "</li>";
         }
         ?>
 
 
         <li>
-            <a href="#submenu2" data-toggle="collapse"><i class="fas fa-list-ul"></i> Menu</a>
-            <ul id="submenu2" class="list-unstyled collapse">
+            <a href="#submenu3" data-toggle="collapse"><i class="fas fa-list-ul"></i> Menu</a>
+            <ul id="submenu3" class="list-unstyled collapse">
                 <li><a href="#"><i class="fas fa-file-alt"></i> Páginas</a></li>
                 <li><a href="#"><i class="fab fa-elementor"></i> Item de Menu</a></li>
             </ul>
