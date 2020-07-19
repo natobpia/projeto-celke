fb589f88add647edf8afed7b5b7d8acc9447efd1
natobpia <natobpia@hotmail.com>
2020-07-19 16:23:22 -0300

    Paginação com PHP e listar níveis

--- celke.sql
+++ celke.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Jul-2020 às 21:22
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.5
+-- Tempo de geração: 19-Jul-2020 às 21:19
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,26 +24,111 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `adms_cors`
+--
+
+CREATE TABLE `adms_cors` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `cor` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `adms_cors`
+--
+
+INSERT INTO `adms_cors` (`id`, `nome`, `cor`, `created`, `modified`) VALUES
+(1, 'Azul', 'primary', '2018-03-23 00:00:00', NULL),
+(2, 'Cinza', 'secondary', '2018-03-23 00:00:00', NULL),
+(3, 'Verde', 'success', '2018-03-23 00:00:00', NULL),
+(4, 'Vermelho', 'danger', '2018-03-23 00:00:00', NULL),
+(5, 'Laranjado', 'warning', '2018-03-23 00:00:00', NULL),
+(6, 'Azul claro', 'info', '2018-03-23 00:00:00', NULL),
+(7, 'Claro', 'light', '2018-03-23 00:00:00', NULL),
+(8, 'Cinza escuro', 'dark', '2018-03-23 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `adms_menus`
+--
+
+CREATE TABLE `adms_menus` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
+  `icone` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `ordem` int(11) NOT NULL,
+  `adms_sit_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `adms_menus`
+--
+
+INSERT INTO `adms_menus` (`id`, `nome`, `icone`, `ordem`, `adms_sit_id`, `created`, `modified`) VALUES
+(1, 'Dashboard', 'fas fa-tachometer-alt', 1, 1, '2018-03-23 00:00:00', NULL),
+(2, 'Usuario', 'fas fa-user', 2, 1, '2018-03-23 00:00:00', NULL),
+(3, 'Menu', 'fas fa-list-ul', 3, 1, '2018-03-23 00:00:00', NULL),
+(4, 'Sair', 'fas fa-sign-out-alt', 4, 1, '2018-03-23 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `adms_nivacs_pgs`
+--
+
+CREATE TABLE `adms_nivacs_pgs` (
+  `id` int(11) NOT NULL,
+  `permissao` int(11) NOT NULL,
+  `ordem` int(11) NOT NULL,
+  `dropdown` int(11) NOT NULL,
+  `lib_menu` int(11) NOT NULL DEFAULT 2,
+  `adms_menu_id` int(11) NOT NULL,
+  `adms_niveis_acesso_id` int(11) NOT NULL,
+  `adms_pagina_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `adms_nivacs_pgs`
+--
+
+INSERT INTO `adms_nivacs_pgs` (`id`, `permissao`, `ordem`, `dropdown`, `lib_menu`, `adms_menu_id`, `adms_niveis_acesso_id`, `adms_pagina_id`, `created`, `modified`) VALUES
+(1, 1, 1, 2, 1, 1, 1, 1, '2018-03-23 00:00:00', NULL),
+(2, 1, 2, 1, 1, 2, 1, 5, '2018-03-23 00:00:00', NULL),
+(3, 1, 3, 1, 1, 2, 1, 6, '2018-03-23 00:00:00', NULL),
+(4, 1, 4, 1, 1, 3, 1, 7, '2018-03-23 00:00:00', NULL),
+(5, 1, 5, 1, 1, 3, 1, 8, '2018-03-23 00:00:00', NULL),
+(6, 1, 6, 2, 1, 4, 1, 4, '2018-03-23 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `adms_niveis_acessos`
 --
 
 CREATE TABLE `adms_niveis_acessos` (
   `id` int(11) NOT NULL,
-  `nome` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
+  `nome` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `ordem` int(11) NOT NULL,
   `created` datetime NOT NULL,
-  `modifed` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `modified` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `adms_niveis_acessos`
 --
 
-INSERT INTO `adms_niveis_acessos` (`id`, `nome`, `ordem`, `created`, `modifed`) VALUES
-(1, 'Super Administrador', 1, '2020-05-27 15:54:47', NULL),
-(2, 'Administrador', 2, '2020-05-27 15:54:47', NULL),
-(3, 'Colaborador', 3, '2020-05-27 15:55:08', NULL),
-(4, 'Cliente', 4, '2020-05-27 15:55:20', NULL);
+INSERT INTO `adms_niveis_acessos` (`id`, `nome`, `ordem`, `created`, `modified`) VALUES
+(1, 'Super Administrador', 1, '2018-03-23 00:00:00', NULL),
+(2, 'Administrador', 2, '2018-03-23 00:00:00', NULL),
+(3, 'Colaborador', 3, '2018-03-23 00:00:00', NULL),
+(4, 'Cliente', 4, '2018-03-23 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -61,22 +146,27 @@ CREATE TABLE `adms_paginas` (
   `author` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `tp_pagina` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `lib_pub` int(11) NOT NULL DEFAULT 2,
-  `ordem` int(11) NOT NULL,
+  `icone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   `depend_pg` int(11) NOT NULL DEFAULT 0,
   `adms_robot_id` int(11) NOT NULL DEFAULT 4,
   `adms_sits_pg_id` int(11) NOT NULL DEFAULT 1,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `adms_paginas`
 --
 
-INSERT INTO `adms_paginas` (`id`, `nome_pagina`, `endereco`, `obs`, `keywords`, `description`, `author`, `tp_pagina`, `lib_pub`, `ordem`, `depend_pg`, `adms_robot_id`, `adms_sits_pg_id`, `created`, `modified`) VALUES
-(1, 'Cadastrar Usuário', 'cadastrar/cad_usuario', 'Página para cadastrar o usuário', 'cadastrar usuário', 'página para cadastrar usuário', 'Natobpia', 'adms', 2, 1, 0, 4, 1, '2020-07-07 10:24:53', NULL),
-(2, 'Login', 'acesso/login', 'Página de login do adm', 'Celke página login', 'Celke página login', 'Natobpia', 'adms', 1, 2, 0, 1, 1, '2020-07-07 11:01:58', NULL),
-(3, 'Validar login', 'acesso/valida', 'Validar login', 'Validar login', 'Validar login', 'Natobpia', 'adms', 1, 3, 2, 4, 1, '2020-07-07 15:26:30', NULL);
+INSERT INTO `adms_paginas` (`id`, `nome_pagina`, `endereco`, `obs`, `keywords`, `description`, `author`, `tp_pagina`, `lib_pub`, `icone`, `depend_pg`, `adms_robot_id`, `adms_sits_pg_id`, `created`, `modified`) VALUES
+(1, 'Home', 'visualizar/home', 'Pagina home', 'home', 'pagina home', 'Celke', 'adms', 2, 'fas fa-tachometer-alt', 0, 4, 1, '2018-03-23 00:00:00', NULL),
+(4, 'Sair', 'acesso/sair', 'Sair do ADM', 'Sair do ADM', 'Sair do ADM', 'Celke', 'adms', 1, 'fas fa-sign-out-alt', 0, 4, 1, '2018-03-23 00:00:00', NULL),
+(2, 'Login', 'acesso/login', 'Pagina de login do ADM', 'celke pagina login', 'celke pagina login', 'Celke', 'adms', 1, NULL, 0, 1, 1, '2018-03-23 00:00:00', NULL),
+(3, 'Validar Login', 'acesso/valida', 'Validar Login', 'Validar Login', 'Validar Login', 'Celke', 'adms', 1, NULL, 2, 4, 1, '2018-03-23 00:00:00', NULL),
+(5, 'Usuarios', 'listar/list_usuario', 'Pagina para listar usuarios', 'Listar usuarios', 'Listar usuarios', 'Celke', 'adms', 2, 'fas fa-users', 0, 4, 1, '2018-03-23 00:00:00', NULL),
+(6, 'Nivel de Acesso', 'listar/list_niv_aces', 'Pagina para listar nivel de acesso', 'Listar nivel de acesso', 'Listar nivel de acesso', 'Celke', 'adms', 2, 'fas fa-key', 0, 4, 1, '2018-03-23 00:00:00', NULL),
+(7, 'Paginas', 'listar/list_pagina', 'Pagina para listar as paginas do ADM', 'Listar pagina', 'Listar pagina', 'Celke', 'adms', 2, 'fas fa-file-alt', 0, 4, 1, '2018-03-23 00:00:00', NULL),
+(8, 'Menu', 'listar/list_menu', 'Pagina para listar os itens do menu', 'Pagina para listar os itens do menu', 'Pagina para listar os itens do menu', 'Celke', 'adms', 2, 'fab fa-elementor', 0, 4, 1, '2018-03-23 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -90,18 +180,41 @@ CREATE TABLE `adms_robots` (
   `tipo` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `adms_robots`
 --
 
 INSERT INTO `adms_robots` (`id`, `nome`, `tipo`, `created`, `modified`) VALUES
-(1, 'Indexar a página e seguir os links', 'index, follow', '2020-07-07 11:14:17', NULL),
-(2, 'Não indexar a página mas seguir os links', 'noindex, follow', '2020-07-07 11:14:17', NULL),
-(3, 'Indexar a página mas não seguir os links', 'index, nofollow', '2020-07-07 11:15:08', NULL),
-(4, 'Não indexar a página e nem seguir os links', 'noindex, nofollow', '2020-07-07 11:15:08', NULL),
-(5, 'Não exibir a versão em cache da página', 'noarchive', '2020-07-07 11:15:57', NULL);
+(1, 'Indexar a pÃ¡gina e seguir os links', 'index, follow', '2018-03-23 00:00:00', NULL),
+(2, 'NÃ£o indexar a pÃ¡gina mas seguir os links', 'noindex, follow', '2018-03-23 00:00:00', NULL),
+(3, 'Indexar a pÃ¡gina mas nÃ£o seguir os links', 'index, nofollow', '2018-03-23 00:00:00', NULL),
+(4, 'NÃ£o indexar a pÃ¡gina e nem seguir os links', 'noindex, nofollow', '2018-03-23 00:00:00', NULL),
+(5, 'NÃ£o exibir a versÃ£o em cache da pÃ¡gina', 'noarchive', '2018-03-23 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `adms_sits`
+--
+
+CREATE TABLE `adms_sits` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `cor` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `adms_sits`
+--
+
+INSERT INTO `adms_sits` (`id`, `nome`, `cor`, `created`, `modified`) VALUES
+(1, 'Ativo', '3', '2018-03-23 00:00:00', NULL),
+(2, 'Inativo', '4', '2018-03-23 00:00:00', NULL),
+(3, 'Analise', '1', '2018-03-23 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -115,16 +228,16 @@ CREATE TABLE `adms_sits_pgs` (
   `cor` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `adms_sits_pgs`
 --
 
 INSERT INTO `adms_sits_pgs` (`id`, `nome`, `cor`, `created`, `modified`) VALUES
-(1, 'Ativo', 'success', '2020-07-07 11:11:13', NULL),
-(2, 'Inativo', 'danger', '2020-07-07 11:11:13', NULL),
-(3, 'Análise', 'primary', '2020-07-07 11:11:48', NULL);
+(1, 'Ativo', 'success', '2018-03-23 00:00:00', NULL),
+(2, 'Inativo', 'danger', '2018-03-23 00:00:00', NULL),
+(3, 'Analise', 'primary', '2018-03-23 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -135,20 +248,20 @@ INSERT INTO `adms_sits_pgs` (`id`, `nome`, `cor`, `created`, `modified`) VALUES
 CREATE TABLE `adms_sits_usuarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
-  `sts_cor_id` int(11) NOT NULL,
+  `adms_cor_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `adms_sits_usuarios`
 --
 
-INSERT INTO `adms_sits_usuarios` (`id`, `nome`, `sts_cor_id`, `created`, `modified`) VALUES
-(1, 'Ativo', 3, '2020-05-27 15:58:12', '0000-00-00 00:00:00'),
-(2, 'Inativo', 5, '2020-05-27 15:58:12', '0000-00-00 00:00:00'),
-(3, 'Aguardando confirmação', 1, '2020-05-27 15:58:36', '0000-00-00 00:00:00'),
-(4, 'Spam', 4, '2020-05-27 15:58:59', '0000-00-00 00:00:00');
+INSERT INTO `adms_sits_usuarios` (`id`, `nome`, `adms_cor_id`, `created`, `modified`) VALUES
+(1, 'Ativo', 3, '2018-03-23 00:00:00', NULL),
+(2, 'Inativo', 5, '2018-03-23 00:00:00', NULL),
+(3, 'Aguardando confirmacao', 1, '2018-03-23 00:00:00', NULL),
+(4, 'Spam', 4, '2018-03-23 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -165,19 +278,19 @@ CREATE TABLE `adms_usuarios` (
   `senha` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
   `recuperar_senha` varchar(220) COLLATE utf8_unicode_ci DEFAULT NULL,
   `chave_descadastro` varchar(220) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `imagem` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
+  `imagem` varchar(220) COLLATE utf8_unicode_ci DEFAULT NULL,
   `adms_niveis_acesso_id` int(11) NOT NULL,
   `adms_sits_usuario_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Extraindo dados da tabela `adms_usuarios`
 --
 
 INSERT INTO `adms_usuarios` (`id`, `nome`, `apelido`, `email`, `usuario`, `senha`, `recuperar_senha`, `chave_descadastro`, `imagem`, `adms_niveis_acesso_id`, `adms_sits_usuario_id`, `created`, `modified`) VALUES
-(1, 'Cesar', 'Cesar', 'cesar@celke.com.br', 'cesar@celke.com.br', '$2y$10$qnErcfIhFoxRFk/6AH1vtO9Hxmp8oRilL1rOlOZIy1.f5Mtr613Wa', NULL, NULL, 'celke.png', 1, 1, '2020-05-27 16:05:10', NULL);
+(1, 'Cesar N. Szpak', 'Cesar', 'cesar@celke.com.br', 'cesar@celke.com.br', '$2y$10$UDdxOqZghWMPVQQ094COZeNdT/VFBJXqwFfAyRNLZnycaXhY8yK9u', NULL, NULL, 'celke.jpg', 1, 1, '2018-03-23 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -628,6 +741,24 @@ INSERT INTO `sts_videos` (`id`, `titulo`, `descricao`, `video_um`, `video_dois`,
 --
 
 --
+-- Índices para tabela `adms_cors`
+--
+ALTER TABLE `adms_cors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `adms_menus`
+--
+ALTER TABLE `adms_menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `adms_nivacs_pgs`
+--
+ALTER TABLE `adms_nivacs_pgs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices para tabela `adms_niveis_acessos`
 --
 ALTER TABLE `adms_niveis_acessos`
@@ -643,6 +774,12 @@ ALTER TABLE `adms_paginas`
 -- Índices para tabela `adms_robots`
 --
 ALTER TABLE `adms_robots`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `adms_sits`
+--
+ALTER TABLE `adms_sits`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -770,6 +907,24 @@ ALTER TABLE `sts_videos`
 --
 
 --
+-- AUTO_INCREMENT de tabela `adms_cors`
+--
+ALTER TABLE `adms_cors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `adms_menus`
+--
+ALTER TABLE `adms_menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `adms_nivacs_pgs`
+--
+ALTER TABLE `adms_nivacs_pgs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de tabela `adms_niveis_acessos`
 --
 ALTER TABLE `adms_niveis_acessos`
@@ -779,13 +934,19 @@ ALTER TABLE `adms_niveis_acessos`
 -- AUTO_INCREMENT de tabela `adms_paginas`
 --
 ALTER TABLE `adms_paginas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `adms_robots`
 --
 ALTER TABLE `adms_robots`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `adms_sits`
+--
+ALTER TABLE `adms_sits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `adms_sits_pgs`
